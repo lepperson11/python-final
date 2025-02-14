1d61effb051bf731258a9a0837dc6520d04e28f1
lepperson11 <lepperson11@gmail.com>
2025-02-14 08:21:47 -0700

    added a login/register system

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
-from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from flask import Flask, render_template, request, url_for, redirect, flash, session
 import json
+from pymongo import MongoClient
+import bcrypt
+from dotenv import load_dotenv
+import os
+import certifi
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.secret_key = os.getenv('SECRETKEY')
+# app.config['MONGO_URI'] = os.getenv('MONGODB_URI')
+# mongo = PyMongo(app,tlsCAFile=certifi.where())
+# records = mongo.db.pythonfinal
 
-class Base(DeclarativeBase):
-    pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-db = SQLAlchemy(model_class=Base)
-db.init_app(app)
+client = MongoClient(os.getenv('MONGODB_URI'), tlsCAFile=certifi.where())
+db = client.get_database('pythonfinal')
+records = db.users
 
-login_manager = LoginManager()
-login_manager.init_app(app)
+# class Base(DeclarativeBase):
+#     pass
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+# db = SQLAlchemy(model_class=Base)
+# db.init_app(app)
 
-@login_manager.user_loader
-def load_user(user_id):
-    return db.get_or_404(User, user_id)
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 
-class User(UserMixin, db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    email: Mapped[str] = mapped_column(String(100), unique=True)
-    password: Mapped[str] = mapped_column(String(100))
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return db.get_or_404(User, user_id)
 
-with app.app_context():
-    db.create_all()
+# class User(UserMixin, db.Model):
+#     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+#     email: Mapped[str] = mapped_column(String(100), unique=True)
+#     password: Mapped[str] = mapped_column(String(100))
+
+# with app.app_context():
+#     db.create_all()
 
 @app.route('/')
 def home():
@@ -102,48 +111,96 @@ def monitors():
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
+    message = 'Please login to your account'
+    if "email" in session:
+        return redirect(url_for("admin"))
+
     if request.method == "POST":
-        email = request.form.get('email')
-        password = request.form.get('password')
+        email = request.form.get("email")
+        password = request.form.get("password")
 
-        result = db.session.execute(db.select(User).where(User.email == email))
-        user = result.scalar()
-
-        if not user:
-            flash("The email or password is incorrect or does not exist")
-            return redirect(url_for('login'))
-        elif not check_password_hash(user.password, password):
-            flash("The email or password is incorrect or does not exist")
-            return redirect(url_for('login'))
+        #check if email exists in database
+        email_found = records.find_one({"email": email})
+        if email_found:
+            email_val = email_found['email']
+            passwordcheck = email_found['password']
+            #encode the password and check if it matches
+            if bcrypt.checkpw(password.encode('utf-8'), passwordcheck):
+                session["email"] = email_val
+                return redirect(url_for('admin'))
+            else:
+                if "email" in session:
+                    return redirect(url_for("admin"))
+                message = 'Wrong password'
+                return render_template('login.html', message=message)
         else:
-            login_user(user)
-            return redirect(url_for('admin'))
+            message = 'Email not found'
+            return render_template('login.html', message=message)
+    # if request.method == "POST":
+    #     email = request.form.get('email')
+    #     password = request.form.get('password')
 
-    return render_template("login.html", logged_in=current_user.is_authenticated)
+    #     result = db.session.execute(db.select(User).where(User.email == email))
+    #     user = result.scalar()
+
+    #     if not user:
+    #         flash("The email or password is incorrect or does not exist")
+    #         return redirect(url_for('login'))
+    #     elif not check_password_hash(user.password, password):
+    #         flash("The email or password is incorrect or does not exist")
+    #         return redirect(url_for('login'))
+    #     else:
+    #         login_user(user)
+    #         return redirect(url_for('admin'))
+
+    return render_template("login.html", message = message)
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
+    message = ""
+
+    if "email" in session:
+        return redirect(url_for("login"))
+    
     if request.method == "POST":
-        email = request.form.get('email')
-        result = db.session.execute(db.select(User).where(User.email == email))
-        user = result.scalar()
-        if user:
-            flash("You already have an account with that email, use log in instead!")
-            return redirect(url_for('login'))
-        hash_salt_pass = generate_password_hash(request.form.get('password'), method="pbkdf2:sha256", salt_length=9)
-        new_user = User(
-            email=request.form.get('email'),
-            password=hash_salt_pass
-        )
-        
-        db.session.add(new_user)
-        db.session.commit()
+        email = request.form.get("email")
+        password = request.form.get("password")
 
-        login_user(new_user)
-        
-        return redirect(url_for("admin"))
+        email_found = records.find_one({"email": email})
+        if email_found:
+            message = "This email already exists in database"
+            return render_template('register.html', message=message)
+        else:
+            hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-    return render_template("register.html", logged_in=current_user.is_authenticated)
+            user_input = {'email': email, 'password': hashed}
+            records.insert_one(user_input)
+
+            user_data = records.find_one({"email": email})
+            new_email = user_data['email']
+            return redirect(url_for('admin'))
+
+    # if request.method == "POST":
+    #     email = request.form.get('email')
+    #     result = db.session.execute(db.select(User).where(User.email == email))
+    #     user = result.scalar()
+    #     if user:
+    #         flash("You already have an account with that email, use log in instead!")
+    #         return redirect(url_for('login'))
+    #     hash_salt_pass = generate_password_hash(request.form.get('password'), method="pbkdf2:sha256", salt_length=9)
+    #     new_user = User(
+    #         email=request.form.get('email'),
+    #         password=hash_salt_pass
+    #     )
+        
+    #     db.session.add(new_user)
+    #     db.session.commit()
+
+    #     login_user(new_user)
+        
+    #     return redirect(url_for("admin"))
+
+    return render_template("register.html")
 
 def row(row_list, name, description, image, alt):
     for row in row_list:
@@ -154,31 +211,36 @@ def row(row_list, name, description, image, alt):
     return name, description, image, alt
 
 @app.route('/admin')
-@login_required
 def admin():
-    with open("./static/json/products.json") as data_file:
-        data = json.load(data_file)
-        accessories_row = data["products"]["accessories"]
-        consoles_row = data["products"]["game_consoles"]
-        computers_row = data["products"]["computers"]
-        games_row = data["products"]["video_games"]
-        monitors_row = data["products"]["monitors"]
-        name_list = []
-        description_list = []
-        image_list = []
-        alt_list = []
-        row(accessories_row, name_list, description_list, image_list, alt_list)
-        row(consoles_row, name_list, description_list, image_list, alt_list)
-        row(computers_row, name_list, description_list, image_list, alt_list)
-        row(games_row, name_list, description_list, image_list, alt_list)
-        row(monitors_row, name_list, description_list, image_list, alt_list)
-        count = len(name_list)
-    return render_template("admin.html", name_list = name_list, description_list = description_list, image_list = image_list, alt_list = alt_list, count = count, logged_in=True)
+    if "email" in session:
+        with open("./static/json/products.json") as data_file:
+            data = json.load(data_file)
+            accessories_row = data["products"]["accessories"]
+            consoles_row = data["products"]["game_consoles"]
+            computers_row = data["products"]["computers"]
+            games_row = data["products"]["video_games"]
+            monitors_row = data["products"]["monitors"]
+            name_list = []
+            description_list = []
+            image_list = []
+            alt_list = []
+            row(accessories_row, name_list, description_list, image_list, alt_list)
+            row(consoles_row, name_list, description_list, image_list, alt_list)
+            row(computers_row, name_list, description_list, image_list, alt_list)
+            row(games_row, name_list, description_list, image_list, alt_list)
+            row(monitors_row, name_list, description_list, image_list, alt_list)
+            count = len(name_list)
+        return render_template("admin.html", name_list = name_list, description_list = description_list, image_list = image_list, alt_list = alt_list, count = count, logged_in=True)
+    else:
+        return redirect(url_for("login"))
 
-@app.route('/logout')
+@app.route('/logout', methods=["POST", "GET"])
 def logout():
-    logout_user()
-    return redirect(url_for('home'))
+    if "email" in session:
+        session.pop("email", None)
+        return render_template("index.html")
+    else:
+        return redirect(url_for('login'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
